68607912150b63a049e956d0702ec42bf428ce45
DiveshThapa986 <122870955+DiveshThapa986@users.noreply.github.com>
2025-07-25 15:01:44 +0530

    Add files via upload

---
+++ java-element/volume of cylinder/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		float radius=sc.nextFloat();
+		float height=sc.nextFloat();
+		double volume=3.14*radius*radius*height;
+		System.out.print(volume);
+	}
+}
